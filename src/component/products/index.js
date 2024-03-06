e7348a2e21564401e79f864225e6ef08c9e731c6
Atul <satul7003@gmail.com>
2024-03-06 23:58:41 +0530

    This is fifth commit

--- src/component/products/index.js
+++ src/component/products/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./style.css";
+import Rating from "@mui/material/Rating";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 const Product = () => {
   return (
     <div className="productthumb">
@@ -9,6 +12,28 @@ const Product = () => {
           className="w-100"
           alt="dd"
         />
+      </div>
+      <div className="info">
+        <span className="d-block catName">Snack</span>
+        <h4 className="title">
+          Seeds of Change Organic Quinaoa,Brown, & Red Rice
+        </h4>
+        <Rating
+          name="half-rating-read"
+          defaultValue={2.5}
+          precision={0.5}
+          readOnly
+        />
+        <span className="brand d-block">
+          By<a className="text-g">NestFood</a>
+        </span>
+        <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center">
+            <span className="price"> $29.44</span>{" "}
+            <span className="oldprice">$32.33</span>
+            <button className="rice"><AddShoppingCartIcon/> Add</button>
+          </div>
+        </div>
       </div>
     </div>
   );
