d384474fde4f275ebeb455c52342cab075f70f55
Atul <satul7003@gmail.com>
2024-03-21 15:08:31 +0530

    Your commit message here

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import "../header/header.css";
 import Logo from "../../assets/images/logo.svg";
 import IconCompare from "../../assets/images/icon-compare.svg";
@@ -52,6 +52,8 @@ const Header = () => {
       console.log("error in get data");
     }
   };
+
+ 
 
   return (
     <>
