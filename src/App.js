097ae6f3a73c46ecc0980dbb30b4b22a17254cd8
Atul <satul7003@gmail.com>
2024-05-22 20:17:50 +0530

    Cart ui developed 70%

--- src/App.js
+++ src/App.js
@@ -7,40 +7,51 @@ import About from "./pages/About/index";
 import Listing from "./pages/Listing";
 import Footer from "./component/footer/footer";
 import Notfound from "./pages/Notfound";
-import Details from "./pages/Details"
+import Details from "./pages/Details";
+import Cart from "./pages/cart";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 function App() {
   const [productData, setProductData] = useState([]);
   useEffect(() => {
     getProducts();
-  }, [])
+  }, []);
   const getProducts = async () => {
     try {
-      await axios.get('http://localhost:3000/productData').then((response) => {
-        setProductData(response.data)
-      })
+      await axios.get("http://localhost:3000/productData").then((response) => {
+        setProductData(response.data);
+      });
+    } catch (error) {
+      alert(error, "error in getting product");
     }
-    catch (error) {
-      alert(error, "error in getting product")
-    }
-  }
+  };
   return (
-    productData.length !== 0 &&
-    <div className="App">
-      <Router>
-        <Header data={productData} />
-        <Routes>
-          <Route path="/" element={<Home data={productData} />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/cat/:id" element={<Listing data={productData} single={true} />} />
-          <Route path="/cat/:id/:id" element={<Listing data={productData} single={false} />} />
-          <Route path="*" element={<Notfound />} />
-          <Route path="products/:id" element={<Details data={productData} />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    productData.length !== 0 && (
+      <div className="App">
+        <Router>
+          <Header data={productData} />
+          <Routes>
+            <Route path="/" element={<Home data={productData} />} />
+            <Route path="/About" element={<About />} />
+            <Route
+              path="/cat/:id"
+              element={<Listing data={productData} single={true} />}
+            />
+            <Route
+              path="/cat/:id/:id"
+              element={<Listing data={productData} single={false} />}
+            />
+            <Route path="*" element={<Notfound />} />
+            <Route
+              path="products/:id"
+              element={<Details data={productData} />}
+            />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    )
   );
 }
 export default App;
